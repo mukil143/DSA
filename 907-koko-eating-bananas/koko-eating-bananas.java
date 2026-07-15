711bddd6bdb387874fa2db59f768f56edd0ee16e
Karmukilan <154080083+mukil143@users.noreply.github.com>
2026-07-15 12:29:46 +0530

    Time: 25 ms (22.27%) | Memory: 47.8 MB (59.72%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1;//1
+        int r = 0;//11
+        for(int pile : piles) 
+            r = Math.max(r,pile);
+
+        while(l < r){
+            int mid = l + (r - l) / 2;//6
+
+            if(canFinish(piles,h,mid)){
+                r = mid;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+    private boolean canFinish(int[] piles,int h, int speed){
+        long hours = 0;
+
+        for(int  pile : piles){
+            hours += Math.ceil((double)pile/speed);
+        }
+
+        return hours <= h;
+    }
+}
